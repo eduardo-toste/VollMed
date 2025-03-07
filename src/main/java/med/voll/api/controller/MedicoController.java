7650afa941c2c358e438776bf2eda4116a000816
eduardo-toste <duciochetti@gmail.com>
2025-03-07 19:25:11 -0300

    criando endpoint para exclusao logica de medico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -33,4 +33,11 @@ public class MedicoController {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }
 }
