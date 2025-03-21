f7c311dec54151878b8bdc4536b7aa9dd3fac7b8
eduardo-toste <duciochetti@gmail.com>
2025-03-20 23:21:27 -0300

    fix: adicionando query faltante

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -4,9 +4,16 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 
-    Boolean findAtivoById(@NotNull Long idPaciente);
+    @Query("""
+            select m.ativo
+            from Paciente P
+            where
+            p.id = :id
+            """)
+    Boolean findAtivoById(@NotNull Long id);
 }
