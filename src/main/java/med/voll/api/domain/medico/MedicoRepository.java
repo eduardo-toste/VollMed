a7f44eadbdec1ecd1e2a52f8cf7e695b93724441
eduardo-toste <duciochetti@gmail.com>
2025-03-20 23:05:40 -0300

    feat: criando metodos para os validadores

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -25,4 +25,12 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             LIMIT 1
             """)
     Medico escolherMedicoAleatorioLivre(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            select m.ativo
+            from Medico m
+            where
+            m.id = :id
+            """)
+    Boolean findAtivoById(Long idMedico);
 }
