e19e9b47a39f0e64cf44775aee7bab36df4a9cc2
eduardo-toste <duciochetti@gmail.com>
2025-03-23 21:23:10 -0300

    feat: especificando as classes para nao haver ambiguidade

--- src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
@@ -4,10 +4,12 @@ import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
 import med.voll.api.infra.exception.ValidacaoException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+@Component("ValidadorHorarioAntecedenciaCancelamento")
 public class ValidadorHorarioAntecedencia implements ValidadorCancelamentoDeConsulta{
 
     @Autowired
