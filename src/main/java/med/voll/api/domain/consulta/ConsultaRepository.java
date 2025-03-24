d7718e2b14135821244e9622c2e28824aa6b0c5e
eduardo-toste <duciochetti@gmail.com>
2025-03-23 21:25:49 -0300

    fix: ajustando bug na consulta se existe medico por id

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -3,12 +3,30 @@ package med.voll.api.domain.consulta;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
-    Boolean existsByMedicoIdAndData(Long aLong, @NotNull @Future LocalDateTime data);
+    @Query("""
+            select m from Medico m
+            where
+            m.ativo = true
+            and
+            m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
+                where
+                c.data = :data
+        and
+                c.motivoCancelamento is null
+            )
+            order by rand()
+            limit 1
+    """)
+    Boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long aLong, LocalDateTime data);
 
     Boolean existsByPacienteIdAndDataBetween(@NotNull Long aLong, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
